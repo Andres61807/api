ea8a790cfb06e6e8d3e693c377f2ba29acecd010
noname <mail@example.mail>
2025-07-23 13:34:14 +0200

    jsonIgnore añadido a editorial

--- src/main/java/tfg/proy/api/entity/Editorial.java
+++ src/main/java/tfg/proy/api/entity/Editorial.java
@@ -2,6 +2,8 @@ package tfg.proy.api.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -35,6 +37,7 @@ public class Editorial {
     private List<LibroEditorial> libros;
     @OneToOne(optional = true)
     @JoinColumn(name="ID_editorial")
+    @JsonIgnore
     private Usuario usuarioFK;
     private String nif;
     
