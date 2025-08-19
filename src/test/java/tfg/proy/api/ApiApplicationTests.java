ef74cefaad6dc3eef973221f58893535c7bf2c8f
noname <noemail@no>
2025-08-19 18:05:15 +0200

    Cambios finales de test

--- src/test/java/tfg/proy/api/ApiApplicationTests.java
+++ src/test/java/tfg/proy/api/ApiApplicationTests.java
@@ -1,31 +1,55 @@
 package tfg.proy.api;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import tfg.proy.api.entity.Autor;
-import tfg.proy.api.entity.Editorial;
-import tfg.proy.api.repository.AutorRepository;
-import tfg.proy.api.repository.EditorialRepository;
-import tfg.proy.api.service.LibroService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import tfg.proy.api.entity.Usuario;
+import tfg.proy.api.repository.UsuarioRepository;;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApiApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Autowired 
+	private UsuarioRepository usuarioRepository;
+	@Autowired 
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+	
+	@Test
+	void testUsuario() throws Exception{
+		Usuario nuevoUsuario=new Usuario();
+		nuevoUsuario.setApellidos("apellidosTest");
+		nuevoUsuario.setCorreo("correoTest");
+		nuevoUsuario.setNombre("nombreTest");	
 
-	// @Autowired
-	// private LibroService libroService;
-	// @Autowired
-	// private EditorialRepository editorialRepository;
-	// @Test
-	// void pruebaAutor(){
-	// 	Editorial editorial=editorialRepository.findByNombre("Libros del Sur");
-	// 	assertNotNull(editorial);
-	// }
+
+		Usuario usuarioGuardado=usuarioRepository.save(nuevoUsuario);
+		
+		MvcResult mvcResult=mockMvc.perform(get("/usuario/"+usuarioGuardado.getId().toString()))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+
+		Usuario usuarioComprobacion=objectMapper.readValue(mvcResult.getResponse().getContentAsString(),Usuario.class);
+		Usuario usuarioComprobacionDB=usuarioRepository.findById(usuarioComprobacion.getId()).orElse(null);
+
+		assertTrue(usuarioComprobacion!=null);
+		assertEquals(usuarioComprobacion.getCorreo(), usuarioComprobacion.getCorreo());
+        
+		assertEquals(usuarioComprobacion.getId(), usuarioComprobacionDB.getId());
+	}
 }
